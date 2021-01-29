6bcfc2cf37424b515509e27b8071227b79a14280
Pedro Lima <pedrogglima@gmail.com>
2021-01-29 16:12:26 -0300

    add default packages & setup bootstrap env

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,25 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
+const webpack = require("webpack");
 
-module.exports = environment
+// Makes $/jQuery global scope/window scope
+environment.config.merge({
+  module: {
+    rules: [
+      {
+        test: require.resolve("jquery"),
+        use: [
+          {
+            loader: "expose-loader",
+            options: "$",
+          },
+          {
+            loader: "expose-loader",
+            options: "jQuery",
+          },
+        ],
+      },
+    ],
+  },
+});
+
+module.exports = environment;
