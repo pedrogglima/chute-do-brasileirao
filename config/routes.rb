c2e451e43f2696b49ac11ad569b20f07ae9f0ec8
Pedro Lima <pedrogglima@gmail.com>
2021-02-05 19:27:51 -0300

    update devise controller, routes and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,19 @@ Rails.application.routes.draw do
   get '/', to: 'index#home'
   get 'sidebar', to: 'index#sidebar'
 
-  devise_for :users
+  scope(path_names: { new: 'novo', edit: 'editar', password: 'senha' }) do
+    devise_for :users,
+      path: 'usuario',
+      class_name: "User",
+      controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations',
+      },
+      path_names: {
+        sign_in: 'entrar',
+        sign_out: 'sair',
+        sign_up: 'cadastrar-se',
+        confirmation: 'verificacao-email',
+      }
+  end
 end
