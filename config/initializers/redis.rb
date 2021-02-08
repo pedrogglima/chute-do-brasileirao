6644d4bf0bc336917c59432d8009007baba03e72
Pedro Lima <pedrogglima@gmail.com>
2021-02-08 15:23:34 -0300

    disable warning message about redis update on sidekiq

---
+++ config/initializers/redis.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+# Redis#exists(key)` will return an Integer in redis-rb 4.3. `exists?` returns a boolean, you should use it instead. To opt-in to the new behavior now you can set Redis.exists_returns_integer =  true. To disable this message and keep the current (boolean) behaviour of 'exists' you can set `Redis.exists_returns_integer = false`, but this option will be removed in 5.0. (/gems/gems/sidekiq-6.0.7/lib/sidekiq/launcher.rb:160:in `block (2 levels)
+Redis.exists_returns_integer = false
