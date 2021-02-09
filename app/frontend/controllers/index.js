9e240974ea66f8f50c6f72a753aa38b043f12bab
Pedro Lima <pedrogglima@gmail.com>
2021-02-09 15:04:37 -0300

    add carousel swiper npm package

--- app/frontend/controllers/index.js
+++ app/frontend/controllers/index.js
@@ -1,9 +1,15 @@
-// Load all the controllers within this directory and all subdirectories. 
+// Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
 
-const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
+// custom npm packages
+import Carousel from "stimulus-carousel";
+
+const application = Application.start();
+const context = require.context("controllers", true, /_controller\.js$/);
+application.load(definitionsFromContext(context));
+
+// custom npm packages
+application.register("carousel", Carousel);
