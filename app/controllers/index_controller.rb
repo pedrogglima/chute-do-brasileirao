370d180af5d6e99bfc6c59710d2878149b0cca4a
Pedro Lima <pedrogglima@gmail.com>
2021-02-15 11:46:25 -0300

    improve performance on queries

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,16 +2,31 @@
 class IndexController < ApplicationController
   def home
     # TODO: add champship year
-    @today_matches = Match.today_matches.order(date: :asc)
-    @next_matches = Match.next_matches(Date.tomorrow).order(date: :asc)
-    @previous_matches = Match.previous_matches(Date.yesterday)
+    @today_matches = Match.team_with_avatar
+      .opponent_with_avatar
+      .today_matches
+      .order(date: :asc)
+
+    @next_matches = Match.team_with_avatar
+      .opponent_with_avatar
+      .next_matches(Date.tomorrow)
+      .order(date: :asc)
+
+    @previous_matches = Match.team_with_avatar
+      .opponent_with_avatar
+      .previous_matches(Date.yesterday)
       .order(date: :desc)
   end
 
   def sidebar
     # TODO: add champship year
-    @top_rankings = Ranking.top_rank.order(posicao: :asc)
-    @bottom_rankings = Ranking.bottom_rank.order(posicao: :desc)
+    @top_rankings = Ranking.team_with_avatar
+      .top_rank
+      .order(posicao: :asc)
+
+    @bottom_rankings = Ranking.team_with_avatar
+      .bottom_rank
+      .order(posicao: :desc)
 
     respond_to do |format|
       format.html do
