bacfc560195e8eadd72385c719369ac57d66fbf5
Pedro Lima <pedrogglima@gmail.com>
2021-02-15 11:51:01 -0300

    improve performance on queries

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 class RankingsController < ApplicationController
+  # TODO: add champship year
   def index
-    @resources = Ranking.all.order(posicao: :asc)
+    @resources = Ranking.all
+      .team_with_avatar
+      .next_opponent_with_avatar
+      .order(posicao: :asc)
   end
 end
