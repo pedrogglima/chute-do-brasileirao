ba70fad6bb62b22daac3e53fca5712d4e4cf1947
Pedro Lima <pedrogglima@gmail.com>
2021-02-15 15:38:31 -0300

    add attr avatar & change faker method that support unique

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -3,7 +3,8 @@ require 'securerandom'
 
 FactoryBot.define do
   factory :team do
-    name { Faker::Sports::Football.team }
+    avatar { FilesTestHelper.jpg }
+    name { Faker::Name.unique.name }
     state { Faker::Address.country_code }
     avatar_url { "https://conteudo.cbf.com.br/cdn/imagens/escudos/00009rs.jpg?v=2021021009" }
   end
