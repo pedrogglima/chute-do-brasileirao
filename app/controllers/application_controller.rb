b419eb9d6e886b65a95a3f99c52191aec02c2ece
Pedro Lima <pedrogglima@gmail.com>
2021-02-16 16:25:14 -0300

    update comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
-  # get current championship from singleton class
+  # rubocop:disable Layout/LineLength
+  #
+  # Define the championship instance used to query rankings, matches, etc, displayed to the user. In other words, users have only access to associations of the current championship.
+  #
+  # rubocop:enable Layout/LineLength
+  #
   def current_championship
     GlobalSetting.singleton.championship
   end
