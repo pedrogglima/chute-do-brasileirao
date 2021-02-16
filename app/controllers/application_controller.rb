b4ba9486f4bc6ac72d86f5b47918f65589004e19
Pedro Lima <pedrogglima@gmail.com>
2021-02-16 15:48:54 -0300

    add current_championship method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   include Pundit
+
+  # get current championship from singleton class
+  def current_championship
+    GlobalSetting.singleton.championship
+  end
 end
