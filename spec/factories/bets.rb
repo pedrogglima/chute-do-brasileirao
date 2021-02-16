f4b8ad8fedd341ef915f877f224efca766658ba5
Pedro Lima <pedrogglima@gmail.com>
2021-02-16 11:13:06 -0300

    update match association

--- spec/factories/bets.rb
+++ spec/factories/bets.rb
@@ -4,7 +4,7 @@ require 'securerandom'
 FactoryBot.define do
   factory :bet do
     association :user
-    association :match
+    association :match, factory: [:match, :not_played_yet]
     bet_team_score { 1 }
     bet_opponent_score { 1 }
   end
