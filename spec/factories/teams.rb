10f2fa5ab936739f1e9e44e99908278e9d2d69c2
Pedro Lima <pedrogglima@gmail.com>
2021-02-23 11:34:33 -0300

    reinfoce unique value for name

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -2,9 +2,11 @@
 require 'securerandom'
 
 FactoryBot.define do
+  unique_name = SecureRandom.hex(16)
+
   factory :team do
     avatar { FilesTestHelper.jpg }
-    name { Faker::Name.unique.name }
+    name { "#{Faker::Name.unique.name}-#{unique_name}" }
     state { Faker::Address.country_code }
     avatar_url { "https://conteudo.cbf.com.br/cdn/imagens/escudos/00009rs.jpg?v=2021021009" }
   end
