59c7e32525a20aa821a15417f31fcc2db6c64177
Pedro Lima <pedrogglima@gmail.com>
2021-02-23 16:34:42 -0300

    update with new redis connect access

--- spec/support/mock_redis_setup.rb
+++ spec/support/mock_redis_setup.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
+
 require 'mock_redis'
 
 module MockRedisSetup
   def mock_redis_setup
     # clean redis db before each spec
-    before(:each) { $redis.redis.flushdb }    
+    redis_conn = redis
+    before(:each) { redis_conn.redis.flushdb }
     yield
+  end
+
+  protected
+
+  def redis
+    Redis.current
   end
 end
