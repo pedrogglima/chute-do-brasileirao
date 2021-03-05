09b0574157523d54200871a1be0d5219c7e99826
Pedro Lima <pedrogglima@gmail.com>
2021-03-05 18:25:00 -0300

    rename specs/services/caches -> specs/caches/*

---
+++ spec/caches/rankings_caches_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe(Cache::RankingsService, type: :service) do
+  let(:global_setting) { create(:global_setting) }
+  let(:championship) { global_setting.championship }
+  let(:cache_resources_service) do
+    Cache::RankingsService.new({ current_championship_id: championship.id })
+  end
+
+  let(:key) { Cache::RankingsService::KEY }
+  let(:exp) { Cache::RankingsService::EXP }
+  let(:from) { Cache::RankingsService::FROM }
+  let(:to) { Cache::RankingsService::TO }
+
+  let(:create_resources) do
+    # p.s create_list syntax didn't set the championship assoc.
+    20.times do |i|
+      create(:ranking, posicao: (i + 1), championship: championship)
+    end
+  end
+
+  mock_redis_setup do
+    describe '#call' do
+      subject { cache_resources_service.call }
+
+      context 'should have constants' do
+        it { expect(key).to eq('ranking_list') }
+        it { expect(exp).to eq(3600) }
+        it { expect(from).to eq(0) }
+        it { expect(to).to eq(-1) }
+      end
+
+      it 'should return' do
+        is_expected.to be_a(Array)
+      end
+
+      context 'when resources do not exist' do
+        it 'should return' do
+          cache_resources = cache_resources_service.call
+          expect(cache_resources.length).to eq(0)
+        end
+      end
+
+      context 'when resources do exist' do
+        it 'should return all list' do
+          create_resources
+          cache_resources = cache_resources_service.call
+          expect(cache_resources.length).to eq(20)
+        end
+      end
+    end
+  end
+end
