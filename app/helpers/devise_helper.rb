d1a113881a1760f0b6a8f06725b41b8374902e01
Pedro Lima <pedrogglima@gmail.com>
2021-03-09 11:22:06 -0300

    add provider_links helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -11,4 +11,27 @@ module DeviseHelper
       'provider não encontrado'
     end
   end
+
+  def provider_links(url, provider)
+    case provider.to_s
+    when 'twitter'
+      twitter_link(url)
+    when 'google_oauth2'
+      google_link(url)
+    else
+      'provider não encontrado'
+    end
+  end
+
+  def twitter_link(url)
+    link_to url, class: 'btn btn-primary btn-block', method: :post do
+      '<i class="fab fa-twitter mr-1"></i><span>Twitter</span>'.html_safe
+    end
+  end
+
+  def google_link(url)
+    link_to url, class: 'btn btn-block btn-google', method: :post do
+      '<i class="fab fa-google mr-1"></i><span>Google</span>'.html_safe
+    end
+  end
 end
