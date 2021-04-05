ef1ab6f15139bf1dd3d6682e47e1c2f4f6917e27
Pedro Lima <pedrogglima@gmail.com>
2021-04-05 18:37:05 -0300

    update scrap_cbf_worker to use scrap_cbf_record

--- app/workers/scrap_cbf_worker.rb
+++ app/workers/scrap_cbf_worker.rb
@@ -4,8 +4,14 @@ class ScrapCbfWorker < ApplicationWorker
   sidekiq_options queue: 'default'
 
   def perform
-    return unless GlobalSetting.singleton.continuing_scraping?
+    gs = GlobalSetting.singleton
 
-    # TODO: create logic
+    return unless gs.continuing_scraping?
+
+    current_championship = gs.championship
+
+    cbf = ScrapCbf.new(year: current_championship.year)
+
+    ScrapCbfRecord::ActiveRecord.save(cbf.to_h)
   end
 end
